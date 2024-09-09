ab212937a3b5271b3dd072ad27b4ced250e2a38a
Jerry Qi <jerryq0101@gmail.com>
2024-09-09 18:03:56 -0400

    Reduce Class

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,21 @@
 public class Reduce {
+    public static void main(String[] args) {
+        // Method
+        System.out.println(reduceSteps(100));
+        System.out.println(reduceSteps(1000));
+    }
+
+    private static int reduceSteps(int number) {
+        int count = 0;
+
+        while (number > 0) {
+            if (number % 2 == 0) {
+                number /= 2; }
+            else {
+                number -=1;
+            }
+            count++;
+        }
+        return count;
+    }
 }
